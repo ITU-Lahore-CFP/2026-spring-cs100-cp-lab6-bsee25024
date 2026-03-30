de68f9b1afae8bdbe7e6c578c1b1cf37e2acf847
bsee25024 <bsee25024@itu.edu.pk>
2026-03-30 18:17:24 +0500

    done alhumdulillah

--- functions.cpp
+++ functions.cpp
@@ -3,10 +3,16 @@
 
 int sumOfSquares(int n) 
 {
+    if(n==0)
+    return 0;
+return(n*n)+sumOfSquares(n-1);
 }
 
 int findFactorial(int n)
 {
+    if(n==0 || n==1)
+    return 1;
+return n*findFactorial(n-1);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include "functions.h"
 
 
-int main() {
-   
+int main() 
+{
+   //Task A
+   int num;
+   cout<<"Enter a number: ";
+   cin>>num;
+   cout<<"Sum of squares: "<<sumOfSquares(num)<<endl;
+
+   //Task B
+   cout<<"\nEnter a number: ";
+   cin>>num;
+   cout<<"Factorial of the number is "<<findFactorial(num)<<endl;
+
+   return 0;
 }
